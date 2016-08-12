65de184bdae07489f0565d42ff20f4b71b772819
ysobj <litailang+ysobj@gmail.com>
2016-08-12 13:15:26 +0900

    use scala.util.Try

--- src/main/scala/CSVReader.scala
+++ src/main/scala/CSVReader.scala
@@ -1,14 +1,22 @@
 import scala.io.Source
+import scala.util.Try
 object CSVReader {
   def main(args: Array[String]): Unit = {
     val file = args(0)
-    val s = Source.fromFile("data/" + file + ".csv")
-    try{
-      for(line <- s.getLines){
-        println(line)
+    val s:Option[Source] = Try{
+      Source.fromFile("data/" + file + ".csv")
+    }.toOption
+    s match{
+      case Some(x) => {
+        try{
+          for(line <- x.getLines){
+            println(line)
+          }
+        }finally{
+          x.close
+        }
       }
-    }finally{
-      s.close
+      case None => println( file + " Not found.")
     }
   }
 }
