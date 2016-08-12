24afbd5e81203c3f3071c4f0d2ba6084ed8ef060
ysobj <litailang+ysobj@gmail.com>
2016-08-12 12:53:10 +0900

    use arguments

--- src/main/scala/CSVReader.scala
+++ src/main/scala/CSVReader.scala
@@ -1,7 +1,8 @@
 import scala.io.Source
 object CSVReader {
   def main(args: Array[String]): Unit = {
-    val s = Source.fromFile("data/STAFF.csv")
+    val file = args(0)
+    val s = Source.fromFile("data/" + file + ".csv")
     try{
       for(line <- s.getLines){
         println(line)
